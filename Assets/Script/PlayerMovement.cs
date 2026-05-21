45a8effe2edf0dfd3f90d48489b29c1ba0b6085b
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2026-05-21 23:46:22 +0200

    BETTER

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,43 +5,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private SOInput soInput;
     [SerializeField] private float maxSpeed = 8f;
     [SerializeField] private float acceleration = 50f;
+    [SerializeField] private float deceleration = 80f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.1f;
 
     private Rigidbody2D rb;
-    private bool jumpRequested;
-    private bool isGrounded;
+    private bool shouldJump;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private bool IsGrounded => Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    private void Start() => rb = GetComponent<Rigidbody2D>();
 
     private void Update()
     {
-        if (Input.GetKeyDown(soInput.JumpKeyCode) && isGrounded)
-            jumpRequested = true;
+        if (Input.GetKeyDown(soInput.JumpKeyCode) && IsGrounded)
+            shouldJump = true;
     }
 
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        float direction = (Input.GetKey(soInput.RightKeyCode) ? 1f : 0f)
+                        - (Input.GetKey(soInput.LeftKeyCode) ? 1f : 0f);
 
-        float direction = 0f;
-        if (Input.GetKey(soInput.RightKeyCode)) direction += 1f;
-        if (Input.GetKey(soInput.LeftKeyCode)) direction -= 1f;
-
-        float targetVelocityX = direction * maxSpeed;
-        float newVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, targetVelocityX, acceleration * Time.fixedDeltaTime);
+        float test = direction != 0f ? acceleration : deceleration;
+        float newVelocityX = Mathf.MoveTowards(rb.linearVelocity.x, direction * maxSpeed, test * Time.fixedDeltaTime);
         rb.linearVelocity = new Vector2(newVelocityX, rb.linearVelocity.y);
 
-        if (jumpRequested)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f); 
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpRequested = false;
-        }
+        if (!shouldJump) return;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        shouldJump = false;
     }
 }
